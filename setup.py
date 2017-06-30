edc16eab666f069cb1fa9c09da7395dca442c5b5
Wittawat Jitkrittum <wittawatj@gmail.com>
2017-06-30 16:39:11 +0100

    add some setup.py related files

---
+++ setup.py
@@ -0,0 +1,84 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kgof',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='Fast kernel-based goodness-of-fit tests',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/wittawatj/kernel-gof',
+
+    # Author details
+    author='Wittawat Jitkrittum',
+    author_email='wittawatj@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+
+    # What does your project relate to?
+    keywords='hypothesis-test kernel-methods machine-learning AI goodness-of-fit',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['data', '*.ex']),
+    
+    # See https://www.python.org/dev/peps/pep-0440/#version-specifiers
+    python_requires='~= 2.7',
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #py_modules=["gofte"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy', 'autograd', 'scipy'],
+)
