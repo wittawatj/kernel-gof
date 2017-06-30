932193c2a2b661d055f4395024577047cbd12a69
Wittawat Jitkrittum <wittawatj@gmail.com>
2017-06-30 16:55:39 +0100

    add matplotlib to setup.py

--- setup.py
+++ setup.py
@@ -80,5 +80,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'autograd', 'scipy'],
+    install_requires=['numpy', 'autograd', 'scipy', 'matplotlib'],
 )
