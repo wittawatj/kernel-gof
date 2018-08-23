minor bug fixes. * use np.linalg.eigh instead of np.linalg.eig for getting the precision matrix in density.Normal * differentiable GaussianMixture density

kgof/data.py
@@ -11,7 +11,6 @@ from future.utils import with_metaclass
 __author__ = 'wittawat'
 
 from abc import ABCMeta, abstractmethod
-import math
 import autograd.numpy as np
 import kgof.util as util
 import scipy.stats as stats
@@ -114,10 +113,14 @@ class DataSource(with_metaclass(ABCMeta, object)):
         the input (n, seed)."""
         raise NotImplementedError()
 
-    #@abstractmethod
-    #def dim(self):
-    #    """Return the dimension of the data. """
-    #    raise NotImplementedError()
+    def dim(self):
+       """
+       Return the dimension of the data.  If possible, subclasses should
+       override this. Determining the dimension by sampling may not be
+       efficient, especially if the sampling relies on MCMC.
+       """
+       dat = self.sample(n=1, seed=3)
+       return dat.dim()
 
 #  end DataSource
 
@@ -216,6 +219,65 @@ class DSIsoGaussianMixture(DataSource):
             assert sample.shape[0] == n
             np.random.shuffle(sample)
         return Data(sample)
+
+# end of class DSIsoGaussianMixture
+
+class DSGaussianMixture(DataSource):
+    """
+    A DataSource implementing a Gaussian mixture in R^d where each component 
+    is an arbitrary Gaussian distribution.
+
+    Let k be the number of mixture components.
+    """
+    def __init__(self, means, variances, pmix=None):
+        """
+        means: a k x d 2d array specifying the means.
+        variances: a k x d x d numpy array containing k covariance matrices,
+            one for each component.
+        pmix: a one-dimensional length-k array of mixture weights. Sum to one.
+        """
+        k, d = means.shape
+        if k != variances.shape[0]:
+            raise ValueError('Number of components in means and variances do not match.')
+
+        if pmix is None:
+            pmix = old_div(np.ones(k),float(k))
+
+        if np.abs(np.sum(pmix) - 1) > 1e-8:
+            raise ValueError('Mixture weights do not sum to 1.')
+
+        self.pmix = pmix
+        self.means = means
+        self.variances = variances
+
+    def sample(self, n, seed=29):
+        pmix = self.pmix
+        means = self.means
+        variances = self.variances
+        k, d = self.means.shape
+        sam_list = []
+        with util.NumpySeedContext(seed=seed):
+            # counts for each mixture component 
+            counts = np.random.multinomial(n, pmix, size=1)
+
+            # counts is a 2d array
+            counts = counts[0]
+
+            # For each component, draw from its corresponding mixture component.            
+            for i, nc in enumerate(counts):
+                # construct the component
+                # https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.multivariate_normal.html
+                cov = variances[i]
+                mnorm = stats.multivariate_normal(means[i], cov)
+                # Sample from ith component
+                sam_i = mnorm.rvs(size=nc)
+                sam_list.append(sam_i)
+            sample = np.vstack(sam_list)
+            assert sample.shape[0] == n
+            np.random.shuffle(sample)
+        return Data(sample)
+
+# end of DSGaussianMixture
 
 
 class DSLaplace(DataSource):
@@ -341,6 +403,10 @@ class DSGaussBernRBM(DataSource):
             return Data(X), H
         else:
             return Data(X)
+
+    def dim(self):
+        return self.B.shape[0]
+
 # end class DSGaussBernRBM
 
 class DSISIPoissonLinear(DataSource):
@@ -714,6 +780,9 @@ class DSResample(DataSource):
         dat = Data(self.X)
         return dat.subsample(n, seed=seed, return_ind = return_ind)
 
+    def dim(self):
+        return self.X.shape[1]
+
 # end class DSResample
 
 class DSGaussCosFreqs(DataSource):
@@ -766,6 +835,9 @@ class DSGaussCosFreqs(DataSource):
                 sam[from_ind:end_ind, :] = X_take
                 from_ind = end_ind
         return Data(sam)
+
+    def dim(self):
+        return len(self.freqs)
 
 
 

kgof/density.py
@@ -13,8 +13,6 @@ __author__ = 'wittawat'
 from abc import ABCMeta, abstractmethod
 import autograd
 import autograd.numpy as np
-import kgof.config as config
-import kgof.util as util
 import kgof.data as data
 import scipy.stats as stats
 #import warnings
@@ -173,6 +171,7 @@ class IsotropicNormal(UnnormalizedDensity):
         return len(self.mean)
 
 
+
 class Normal(UnnormalizedDensity):
     """
     A multivariate normal distribution.
@@ -186,7 +185,7 @@ class Normal(UnnormalizedDensity):
         self.cov = cov
         assert mean.shape[0] == cov.shape[0]
         assert cov.shape[0] == cov.shape[1]
-        E, V = np.linalg.eig(cov)
+        E, V = np.linalg.eigh(cov)
         if np.any(np.abs(E) <= 1e-7):
             raise ValueError('covariance matrix is not full rank.')
         # The precision matrix
@@ -288,6 +287,83 @@ class IsoGaussianMixture(UnnormalizedDensity):
         k, d = self.means.shape
         return d
 
+# end class IsoGaussianMixture
+
+class GaussianMixture(UnnormalizedDensity):
+    """
+    UnnormalizedDensity of a Gaussian mixture in R^d where each component 
+    can be arbitrary. This is the most general form of a Gaussian mixture.
+
+    Let k be the number of mixture components.
+    """
+    def __init__(self, means, variances, pmix=None):
+        """
+        means: a k x d 2d array specifying the means.
+        variances: a k x d x d numpy array containing a stack of k covariance
+            matrices, one for each mixture component.
+        pmix: a one-dimensional length-k array of mixture weights. Sum to one.
+        """
+        k, d = means.shape
+        if k != variances.shape[0]:
+            raise ValueError('Number of components in means and variances do not match.')
+
+        if pmix is None:
+            pmix = old_div(np.ones(k),float(k))
+
+        if np.abs(np.sum(pmix) - 1) > 1e-8:
+            raise ValueError('Mixture weights do not sum to 1.')
+
+        self.pmix = pmix
+        self.means = means
+        self.variances = variances
+
+    def log_den(self, X):
+        return self.log_normalized_den(X)
+
+    def log_normalized_den(self, X):
+        pmix = self.pmix
+        means = self.means
+        variances = self.variances
+        k, d = self.means.shape
+        n = X.shape[0]
+
+        den = np.zeros(n, dtype=float)
+        for i in range(k):
+            norm_den_i = GaussianMixture.multivariate_normal_density(means[i],
+                    variances[i], X)
+            den = den + norm_den_i*pmix[i]
+        return np.log(den)
+
+    @staticmethod
+    def multivariate_normal_density(mean, cov, X):
+        """
+        Exact density (not log density) of a multivariate Gaussian.
+        mean: length-d array
+        cov: a dxd covariance matrix
+        X: n x d 2d-array
+        """
+        
+        evals, evecs = np.linalg.eig(cov)
+        cov_half_inv = evecs.dot(np.diag(evals**(-0.5))).dot(evecs.T)
+    #     print(evals)
+        half_evals = np.dot(X-mean, cov_half_inv)
+        full_evals = np.sum(half_evals**2, 1)
+        unden = np.exp(-0.5*full_evals)
+        
+        Z = np.sqrt(np.linalg.det(2.0*np.pi*cov))
+        den = unden/Z
+        assert len(den) == X.shape[0]
+        return den
+
+    def get_datasource(self):
+        return data.DSGaussianMixture(self.means, self.variances, self.pmix)
+
+    def dim(self):
+        k, d = self.means.shape
+        return d
+
+# end GaussianMixture
+
 class GaussBernRBM(UnnormalizedDensity):
     """
     Gaussian-Bernoulli Restricted Boltzmann Machine.
@@ -346,8 +422,8 @@ class GaussBernRBM(UnnormalizedDensity):
         S = self.b - X + T
         return S
 
-    def get_datasource(self):
-        return data.DSGaussBernRBM(self.B, self.b, self.c)
+    def get_datasource(self, burnin=2000):
+        return data.DSGaussBernRBM(self.B, self.b, self.c, burnin=burnin)
 
     def dim(self):
         return len(self.b)

kgof/goftest.py
@@ -312,6 +312,7 @@ class FSSD(GofTest):
         #Xi = (grad_logp_K + dKdV)
         return Xi
 
+
     @staticmethod
     def power_criterion(p, dat, k, test_locs, reg=1e-2, use_unbiased=True, 
             use_2terms=False):
@@ -388,9 +389,6 @@ class FSSD(GofTest):
         Simulate the null distribution using the spectrums of the covariance
         matrix.  This is intended to be used to approximate the null
         distribution.
-
-        - features_x: n x Dx where Dx is the number of features for x 
-        - features_y: n x Dy
 
         Return (a numpy array of simulated n*FSSD values, eigenvalues of cov)
         """
@@ -628,7 +626,7 @@ class GaussFSSD(FSSD):
               tol=tol_fun, 
               options={
                   'maxiter': max_iter, 'ftol': tol_fun, 'disp': disp,
-                  'gtol': 1.0e-06,
+                  'gtol': 1.0e-07,
                   },
               jac=grad_obj,
             )
@@ -1105,4 +1103,68 @@ class LinearKernelSteinTest(GofTest):
             return stat, H
         else:
             return stat
+
+# end LinearKernelSteinTest
+
+class SteinWitness(object):
+    """
+    Construct a callable object representing the Stein witness function. 
+    The witness function g is defined as in Eq. 1 of 
+
+        A Linear-Time Kernel Goodness-of-Fit Test
+        Wittawat Jitkrittum, Wenkai Xu, Zoltan Szabo, Kenji Fukumizu,
+        Arthur Gretton
+        NIPS 2017
+
+    The witness function requires taking an expectation over the sample
+    generating distribution. This is approximated by an empirical
+    expectation using the sample in the input (dat). The witness function
+    is a d-variate (d = dimension of the data) function, which depends on 
+    the kernel k and the model p.
+
+    The constructed object can be called as if it is a function: (J x d) numpy
+    array |-> (J x d) outputs 
+    """
+    def __init__(self, p, k, dat):
+        """
+        :params p: an UnnormalizedDensity object
+        :params k: a DifferentiableKernel
+        :params dat: a kgof.data.Data
+        """
+        self.p = p
+        self.k = k
+        self.dat = dat
+
+    def __call__(self, V):
+        """
+        :params V: a numpy array of size J x d (data matrix)
+
+        :returns (J x d) numpy array representing witness evaluations at the J
+            points.
+        """
+        J = V.shape[0]
+        X = self.dat.data()
+        n, d = X.shape
+        # construct the feature tensor (n x d x J)
+        fssd = FSSD(self.p, self.k, V, null_sim=None, alpha=None)
+
+        # When X, V contain many points, this can use a lot of memory.
+        # Process chunk by chunk.
+        block_rows = util.constrain(50000//(d*J), 10, 5000)
+        avg_rows = []
+        for (f, t) in util.ChunkIterable(start=0, end=n, chunk_size=block_rows):
+            assert f<t
+            Xblock = X[f:t, :]
+            b = Xblock.shape[0]
+            F = fssd.feature_tensor(Xblock)
+            Tau = np.reshape(F, [b, d*J])
+            # witness evaluations computed on only a subset of data
+            avg_rows.append(Tau.mean(axis=0))
+
+        # an array of length d*J
+        witness_evals = (float(b)/n)*np.sum(np.vstack(avg_rows), axis=0)
+        assert len(witness_evals) == d*J
+        return np.reshape(witness_evals, [J, d])
+
+
 

kgof/util.py
@@ -1,7 +1,14 @@
 """A module containing convenient methods for general machine learning"""
 from __future__ import print_function
 from __future__ import division
+from __future__ import unicode_literals
+from __future__ import absolute_import
 
+from builtins import zip
+from builtins import int
+from builtins import range
+from future import standard_library
+standard_library.install_aliases()
 from past.utils import old_div
 from builtins import object
 __author__ = 'wittawat'
@@ -54,6 +61,33 @@ class NumpySeedContext(object):
     def __exit__(self, *args):
         np.random.set_state(self.cur_state)
 
+# end NumpySeedContext
+
+class ChunkIterable(object):
+    """
+    Construct an Iterable such that each call to its iterator returns a tuple
+    of two indices (f, t) where f is the starting index, and t is the ending
+    index of a chunk. f and t are (chunk_size) apart except for the last tuple
+    which will always cover the rest.
+    """
+    def __init__(self, start, end, chunk_size):
+        self.start = start
+        self.end = end
+        self.chunk_size = chunk_size
+    
+    def __iter__(self):
+        s = self.start
+        e = self.end
+        c = self.chunk_size
+        # Probably not a good idea to use list. Waste memory.
+        L = list(range(s, e, c))
+        L.append(e)
+        return zip(L, L[1:])
+
+# end ChunkIterable
+
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
 
 def dist_matrix(X, Y):
     """
@@ -147,11 +181,8 @@ def subsample_ind(n, k, seed=32):
     """
     Return a list of indices to choose k out of n without replacement
     """
-    rand_state = np.random.get_state()
-    np.random.seed(seed)
-
-    ind = np.random.choice(n, k, replace=False)
-    np.random.set_state(rand_state)
+    with NumpySeedContext(seed=seed):
+        ind = np.random.choice(n, k, replace=False)
     return ind
 
 def subsample_rows(X, k, seed=29):